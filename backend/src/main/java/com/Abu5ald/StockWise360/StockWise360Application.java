3bbcfeff8267b36801be71c226c087a6e50d3dc7
Mohamed Khaled <mohamedkhaled7502@gmail.com>
2025-08-17 00:21:37 +0300

    Initial commit: roject setup

---
+++ backend/src/main/java/com/Abu5ald/StockWise360/StockWise360Application.java
@@ -0,0 +1,13 @@
+package com.Abu5ald.StockWise360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWise360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockWise360Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Abu5ald/StockWise360/StockWise360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Abu5ald.StockWise360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockWise360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
